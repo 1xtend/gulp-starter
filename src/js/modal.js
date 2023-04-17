9be0ef248f2f02c72b444fcb37cfe5b5833353e3
1xtend <vlad.mezhevyi@gmail.com>
2023-04-17 15:23:07 +0300

    upgraded modal, added comments

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,18 +1,29 @@
 /*
-? Button example
+Button example
 
 <button type="button" data-modal="modal_1">Open</button>
 */
 
 export const modal = () => {
-  const modalTogglers = document.querySelectorAll('[data-modal]');
+  // Vars
+  const html = document.documentElement;
   const body = document.body;
+
+  const modalTogglers = document.querySelectorAll('[data-modal]');
   const lockPadding = document.querySelectorAll('lock-padding');
 
+  const transitionValue = 300;
+
+  // Is required to save scroll position
+  let scrollPosition = 0;
+
+  // Is required for locking body
   let unlock = true;
 
-  const timeout = 300;
+  // Is required for closing modal by Escape
+  let isModalActive = false;
 
+  // Event listeners on elements with data-modal attribute
   if (modalTogglers.length > 0) {
     modalTogglers.forEach((modalToggler) => {
       modalToggler.addEventListener('click', (e) => {
@@ -20,11 +31,15 @@ export const modal = () => {
 
         const targetId = modalToggler.dataset.modal;
         const currentModal = document.querySelector(`#${targetId}`);
+
         modalOpen(currentModal);
+
+        saveScroll();
       });
     });
   }
 
+  // Close modal by click on element with close-modal class
   const modalCloseBtns = document.querySelectorAll('.close-modal');
   if (modalCloseBtns.length > 0) {
     modalCloseBtns.forEach((closeBtn) => {
@@ -36,9 +51,11 @@ export const modal = () => {
     });
   }
 
+  // Open modal
   function modalOpen(currentModal) {
     if (currentModal && unlock) {
       const modalActive = document.querySelector('.modal.is-active');
+      isModalActive = true;
 
       if (modalActive) {
         modalClose(modalActive, false);
@@ -55,16 +72,20 @@ export const modal = () => {
     }
   }
 
+  // Close modal
   function modalClose(modalActive, doUnlock = true) {
     if (unlock) {
       modalActive.classList.remove('is-active');
+      isModalActive = false;
 
       if (doUnlock) {
         bodyUnLock();
+        resetScroll();
       }
     }
   }
 
+  // Lock body on desktop and mobile devices
   function bodyLock() {
     const lockPaddingValue =
       window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
@@ -76,14 +97,15 @@ export const modal = () => {
     }
 
     body.style.paddingRight = lockPaddingValue;
-    body.classList.add('is-fixed');
+    body.classList.add('is-locked');
 
     unlock = false;
     setTimeout(() => {
       unlock = true;
-    }, timeout);
+    }, transitionValue);
   }
 
+  // Unlock body on desktop and mobile devices
   function bodyUnLock() {
     setTimeout(() => {
       if (lockPadding.length > 0) {
@@ -93,17 +115,34 @@ export const modal = () => {
       }
 
       body.style.paddingRight = '0px';
-      body.classList.remove('is-fixed');
-    }, timeout);
+      body.classList.remove('is-locked');
+    }, transitionValue);
 
     unlock = false;
     setTimeout(() => {
       unlock = true;
-    }, timeout);
+    }, transitionValue);
   }
 
-  document.addEventListener('keydown', (e) => {
-    if (e.code === 'Escape') {
+  // Save element position
+  function saveScroll() {
+    scrollPosition = window.pageYOffset;
+    html.style.position = 'fixed';
+    html.style.top = -scrollPosition + 'px';
+  }
+
+  // Scroll to saved position and reset saved value
+  function resetScroll() {
+    setTimeout(() => {
+      html.style.top = '';
+      html.style.position = 'relative';
+      window.scrollTo(0, scrollPosition);
+    }, transitionValue);
+  }
+
+  // Close modal by clicking on Escape key
+  document.addEventListener('keyup', (e) => {
+    if (e.code === 'Escape' && isModalActive) {
       const modalActive = document.querySelector('.modal.is-active');
       modalClose(modalActive);
     }
