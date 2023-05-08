f5f3f7b605f25905749f710733dce77d1c3c8d70
1xtend <vlad.mezhevyi@gmail.com>
2023-05-08 23:33:44 +0300

    improved tabs

--- src/js/dev/tabs.js
+++ src/js/dev/tabs.js
@@ -1,48 +1,60 @@
-/*
-? SCSS styles
-
-.tab-toggler example {
-  &.active-tab-btn {
-    ? Example
-    border-color: #f2f2f2;
-  }
-}
-
-.tab-content example {
-  display: none;
-
-  &.active-tab {
-    display: block;
-  }
-}
-
-*/
-
 export const tabs = () => {
-  const tabs = document.querySelectorAll('.tabs');
+  // All Tab Wrappers which containing tabs.
+  const tabWrappers = document.querySelectorAll('[data-tab-wrapper]');
 
-  if (tabs.length > 0) {
-    tabs.forEach((tab) => {
-      tab.addEventListener('click', (e) => {
-        handleTab(tab, e);
-      });
-    });
+  let tabWrappersLength = tabWrappers.length;
 
-    function handleTab(tab, e) {
-      e.preventDefault();
+  // Check if Tab Wrapper(s) exist.
+  if (tabWrappersLength > 0) {
+    // Function that finds Tab Content by data attribute and returns it.
+    function findTabContent(targetId, tabWrapper) {
+      return tabWrapper.querySelector(`[data-tab-content="${targetId}"]`);
+    }
 
-      if (e.target.closest('.active-tab-btn') || !e.target.closest('[data-tab-target]')) {
-        return;
+    // Show Tab function.
+    function showTab(tabToggler, tabContent, tabWrapper) {
+      // Find Active Tab.
+      let activeTab = tabWrapper.querySelector('.active-tab-toggler');
+
+      // Check if Active Tab exists.
+      if (activeTab) {
+        // Find Active Tab Content.
+        const activeTabContent = findTabContent(activeTab.dataset.showTab, tabWrapper);
+
+        // Hide Active Tab.
+        hideTab(activeTab, activeTabContent);
       }
 
-      tab.querySelector('.active-tab-btn').classList.remove('active-tab-btn');
-      tab.querySelector('.active-tab').classList.remove('active-tab');
-
-      const tabToggler = e.target.closest('[data-tab-target]');
-      const tabContent = tab.querySelector(`#${tabToggler.dataset.tabTarget}`);
-
-      tabToggler.classList.add('active-tab-btn');
-      tabContent.classList.add('active-tab');
+      // Show Tab.
+      tabToggler.classList.add('active-tab-toggler');
+      tabContent.classList.add('active-tab-content');
     }
+
+    // Hide Tab function.
+    function hideTab(tabToggler, tabContent) {
+      tabToggler.classList.remove('active-tab-toggler');
+      tabContent.classList.remove('active-tab-content');
+    }
+
+    tabWrappers.forEach((tabWrapper) => {
+      // Add event listeners on Tabs Wrappers.
+      tabWrapper.addEventListener('click', (e) => {
+        // Stop default action.
+        e.preventDefault();
+
+        // Stop function if e.target is not equal to data-show-tab or .active-tab-toggler.
+        if (!e.target.closest('[data-show-tab]') || e.target.closest('.active-tab-toggler')) {
+          return;
+        }
+
+        // Find Tab Toggler and Tab Content
+        const tabToggler = e.target.closest('[data-show-tab]');
+        const tabContentId = tabToggler.dataset.showTab;
+        const tabContent = findTabContent(tabContentId, tabWrapper);
+
+        // Show Tab
+        showTab(tabToggler, tabContent, tabWrapper);
+      });
+    });
   }
 };
