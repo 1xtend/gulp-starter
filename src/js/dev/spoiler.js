de0a1250d33b4b2654c0435ea9e625540de480ef
1xtend <vlad.mezhevyi@gmail.com>
2023-05-08 22:34:52 +0300

    improved spoilers

--- src/js/dev/spoiler.js
+++ src/js/dev/spoiler.js
@@ -46,34 +46,57 @@ CSS
 */
 
 export const spoiler = () => {
-  const spoilers = document.querySelectorAll('[data-spoiler]');
+  // All Spoiler Wrappers containing spoilers.
+  const spoilerWrappers = document.querySelectorAll('[data-spoilers-wrapper]');
 
-  if (spoilers.length > 0) {
-    spoilers.forEach((spoiler) => {
-      spoiler.addEventListener('click', (e) => {
+  let spoilerWrappersLength = spoilerWrappers.length;
+
+  // Check if Spoiler Wrapper(s) exist.
+  if (spoilerWrappersLength > 0) {
+    // Show spoiler function.
+    function showSpoiler(spoiler, spoilerContent, activeSpoiler) {
+      // Check if activeSpoiler exist.
+      if (activeSpoiler) {
+        // Find content inside active spoiler.
+        let activeSpoilerContent = activeSpoiler.querySelector('[data-spoiler-content]');
+
+        // Close active spoiler.
+        hideSpoiler(activeSpoiler, activeSpoilerContent);
+      }
+
+      // Show spoiler.
+      spoiler.classList.add('spoiler-active');
+      spoilerContent.style.maxHeight = `${spoilerContent.scrollHeight}px`;
+    }
+
+    // Hide spoiler function.
+    function hideSpoiler(spoiler, spoilerContent) {
+      // Hide spoiler.
+      spoiler.classList.remove('spoiler-active');
+      spoilerContent.style.maxHeight = 0;
+    }
+
+    spoilerWrappers.forEach((spoilerWrapper) => {
+      // Add event listeners on Spoiler Wrappers.
+      spoilerWrapper.addEventListener('click', (e) => {
+        // Stop function if e.target is not equal to data-spoiler-toggler.
         if (!e.target.closest('[data-spoiler-toggler]')) {
           return;
         }
 
-        const spoilerContent = spoiler.querySelector('[data-spoiler-content]');
+        // Find spoiler and spoiler content.
+        const spoiler = e.target.closest('[data-spoiler]');
+        const spoilerContent = e.target.nextElementSibling;
 
         if (!spoiler.classList.contains('spoiler-active')) {
-          const activeSpoiler = document.querySelector('.spoiler-active');
+          // Find active spoiler.
+          let activeSpoiler = spoilerWrapper.querySelector('.spoiler-active');
 
-          if (activeSpoiler) {
-            activeSpoiler.classList.remove('spoiler-active');
-
-            const activeSpoilerContent = activeSpoiler.querySelector('[data-spoiler-content]');
-            activeSpoilerContent.style.maxHeight = 0;
-            activeSpoilerContent.removeAttribute('style');
-          }
-
-          spoiler.classList.add('spoiler-active');
-          spoilerContent.style.maxHeight = spoilerContent.scrollHeight + 'px';
+          // Show spoiler.
+          showSpoiler(spoiler, spoilerContent, activeSpoiler);
         } else {
-          spoiler.classList.remove('spoiler-active');
-          spoilerContent.style.maxHeight = 0;
-          spoilerContent.removeAttribute('style');
+          // Hide spoiler.
+          hideSpoiler(spoiler, spoilerContent);
         }
       });
     });
