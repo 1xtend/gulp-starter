fba4b294aa1bc4386bb2f74212154b2a5926500b
1xtend <vlad.mezhevyi@gmail.com>
2023-05-10 13:46:26 +0300

    added body lock

---
+++ src/js/dev/bodyLock.js
@@ -0,0 +1,56 @@
+import { mainElems } from '../app.js';
+
+let scrollPosition;
+let bodyPadding = 0;
+
+// All elements with position: fixed;
+let fixedElems = document.querySelectorAll('.fixed');
+
+let fixedElemsLength = fixedElems.length;
+
+// Body lock function.
+export const bodyLock = () => {
+  // Save current position.
+  scrollPosition = window.scrollY;
+  // Changed scroll behavior (because can make bugs).
+  mainElems.html.style.scrollBehavior = 'unset';
+
+  // Save scrollbar width.
+  bodyPadding = window.innerWidth - mainElems.body.offsetWidth + 'px';
+
+  // Add scroll position.
+  mainElems.body.style.top = `-${scrollPosition}px`;
+  // Add padding right to body.
+  mainElems.body.style.paddingRight = bodyPadding;
+
+  // Check if fixed elements exist.
+  if (fixedElemsLength > 0) {
+    fixedElems.forEach((fixedElem) => {
+      // Add padding right to fixed elements.
+      fixedElem.style.paddingRight = bodyPadding;
+    });
+  }
+
+  // Add body-lock class to body.
+  mainElems.body.classList.add('body-lock');
+};
+
+// Body unlock function.
+export const bodyUnLock = () => {
+  // Remove all changes.
+  mainElems.body.classList.remove('body-lock');
+  mainElems.body.style.top = '';
+  mainElems.body.style.paddingRight = '';
+  mainElems.html.style.scrollBehavior = '';
+
+  // Check if fixed elements exist.
+  if (fixedElemsLength > 0) {
+    fixedElems.forEach((fixedElem) => {
+      // Remove padding right
+      fixedElem.style.paddingRight = '';
+    });
+  }
+
+  // Scroll to saved position
+  window.scrollTo(0, scrollPosition);
+};
