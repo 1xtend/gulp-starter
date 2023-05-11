f923ff4d10af385e302dfbc08d421f6bb58efbe5
1xtend <vlad.mezhevyi@gmail.com>
2023-05-11 12:44:02 +0300

    improved burger function

--- src/js/dev/burger.js
+++ src/js/dev/burger.js
@@ -1,5 +1,6 @@
 // Import body and html
 import { mainElems } from '../app.js';
+import { bodyLock, bodyUnLock } from './bodyFix.js';
 
 /*
 
@@ -13,35 +14,50 @@ EXAMPLE
 
 // Burger
 export const burger = () => {
+  // Find a burger.
   const burger = document.querySelector('.burger');
-  const navbar = document.querySelector('.navbar');
 
-  // If burger exists
+  // If burger exists.
   if (burger) {
-    // Open and Close navbar by clicking on a burger
-    burger.addEventListener('click', (e) => {
-      addClasses();
-    });
+    const navbar = document.querySelector('.navbar');
 
-    // Close navbar by clicking on a link
-    navbar.addEventListener('click', (e) => {
-      if (e.target.classList.contains('navbar__link')) {
+    // Event listener on a burger
+    burger.addEventListener('click', (e) => {
+      // If burger has is-active class hide menu, else show menu.
+      if (!burger.classList.contains('is-active')) {
+        addClasses();
+        bodyLock();
+      } else {
         removeClasses();
+        bodyUnLock();
       }
     });
-  }
 
-  // Add Classes
-  function addClasses() {
-    mainElems.body.classList.toggle('is-fixed');
-    burger.classList.toggle('is-active');
-    navbar.classList.toggle('is-active');
-  }
+    // Add classes function.
+    function addClasses() {
+      burger.classList.add('is-active');
+      navbar.classList.add('is-active');
+      navbar.addEventListener('click', handleNavbarClick);
+    }
 
-  // Remove Classes
-  function removeClasses() {
-    mainElems.body.classList.remove('is-fixed');
-    burger.classList.remove('is-active');
-    navbar.classList.remove('is-active');
+    // Remove classes function.
+    function removeClasses() {
+      burger.classList.remove('is-active');
+      navbar.classList.remove('is-active');
+      navbar.removeEventListener('click', handleNavbarClick);
+    }
+
+    // Close menu by click on navbar link function.
+    function handleNavbarClick(e) {
+      if (e.target.closest('.navbar__link')) {
+        // Don't unlock body if e.target is a btn that open modal.
+        if (!e.target.closest('[data-open-modal]')) {
+          bodyUnLock();
+        }
+
+        // Remove classes.
+        removeClasses();
+      }
+    }
   }
 };
